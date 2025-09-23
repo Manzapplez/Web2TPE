3b2537565d2d03908ecac10d93e15b329146b795
Martín <136357272+07martin12@users.noreply.github.com>
2025-09-23 13:34:00 -0300

    Base de datos y tablas creadas con éxito.
    Faltan cargar datos en las tablas de "Artistis" y "Songs"

---
+++ soundSnack.sql
@@ -0,0 +1,86 @@
+-- MySQL dump 10.13  Distrib 8.0.43, for Linux (x86_64)
+--
+-- Host: localhost    Database: SoundSnack
+-- ------------------------------------------------------
+-- Server version	8.0.43-0ubuntu0.24.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Artists`
+--
+
+DROP TABLE IF EXISTS `Artists`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Artists` (
+  `id_artist` smallint unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `biography` text,
+  `cover` varchar(255) DEFAULT NULL,
+  `date_of_birth` date NOT NULL,
+  `date_of_death` date DEFAULT NULL,
+  `place_of_birth` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_artist`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Artists`
+--
+
+LOCK TABLES `Artists` WRITE;
+/*!40000 ALTER TABLE `Artists` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Artists` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Songs`
+--
+
+DROP TABLE IF EXISTS `Songs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Songs` (
+  `id_song` smallint unsigned NOT NULL AUTO_INCREMENT,
+  `id_artist` smallint unsigned NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `album` varchar(100) NOT NULL,
+  `duration` decimal(4,2) DEFAULT NULL,
+  `genre` varchar(50) NOT NULL,
+  `video` varchar(255) DEFAULT NULL,
+  `cover` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_song`),
+  KEY `id_artist` (`id_artist`),
+  CONSTRAINT `Songs_ibfk_1` FOREIGN KEY (`id_artist`) REFERENCES `Artists` (`id_artist`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Songs`
+--
+
+LOCK TABLES `Songs` WRITE;
+/*!40000 ALTER TABLE `Songs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Songs` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-09-23 13:26:14
